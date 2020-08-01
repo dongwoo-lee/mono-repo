1ac6d3ab4a94dcd99bda625734d1ec296762e339
chaesoo.han <chaesoo.han@adsoft.kr>
2020-08-01 17:03:53 +0900

    2020-08-01 백엔드(CM포맷에러수정)

--- MAMBrowser/DTO/Products/DTO_CM.cs
+++ MAMBrowser/DTO/Products/DTO_CM.cs
@@ -12,7 +12,7 @@ namespace MAMBrowser.DTO
         public string ID { get; set; }          //그룹ID
         public string Name { get; set; }    //그룹명
         public string Length { get; set; }      //길이 (xx:xx)
-        public string Capacity { get; set; }    //용량 (int)
+        public int Capacity { get; set; }    //용량 (int)
         public string Status { get; set; }      //상태 (코드)
         public string EditorID { get; set; }      //담당자ID
         public string EditorName { get; set; }   //담당자명
