0bad0d67f17effd47333e3acab2bd3a8e6f72a64
dongwoo.lee <dongwoo.lee@adsoft.kr>
2020-12-26 01:05:53 +0900

    2020-12-26 프론트: css정리 및 font family 확인

--- MAMBrowser/clientapp/src/index.js
+++ MAMBrowser/clientapp/src/index.js
@@ -1,4 +1,3 @@
-import "./assets/css/vendor/dropzone.min.css";
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import "./assets/css/vendor/bootstrap.min.css";
 import "./assets/css/vendor/bootstrap.rtl.only.min.css";
