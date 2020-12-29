c2666d20992b969c3374ad2548553bf46cfe3986
dongwoo.lee <dongwoo.lee@adsoft.kr>
2020-12-29 23:02:25 +0900

    백엔드 : My공간 복사 업무로직변경 2(스트림 파일 사이즈관련)

--- MAMBrowser/BLL/PrivateFileBLL.cs
+++ MAMBrowser/BLL/PrivateFileBLL.cs
@@ -25,8 +25,12 @@ namespace MAMBrowser.BLL
             _dal = dal;
         }
 
-        public long UploadFile(string userId, Stream stream, string fileName,PrivateFileModel metaData)
+        public long UploadFile(string userId, Stream stream, string fileName, PrivateFileModel metaData)
         {
+            // 파일 크기로 유효성검사 필요.
+            // private, public은 ifromfile객체의 사이즈를 사용.
+            // My공간 복사 시 -> 파일다쓰고 파일사이즈 확인함.(FTP)
+
             long ID = _dal.GetID();
             string date = DateTime.Now.ToString(MAMUtility.DTM8);
             string newFileName = $"{ ID.ToString() }_{fileName}";
@@ -40,17 +44,18 @@ namespace MAMBrowser.BLL
             var audioFormat = AudioEngine.GetAudioFormat(headerStream, relativeTargetPath);
             headerStream.Position = 0;
             _fileService.MakeDirectory(relativeSourceFolder);
-            _fileService.Upload(headerStream, stream, relativeSourcePath, stream.Length);
+            _fileService.Upload(headerStream, stream, relativeSourcePath);
             _fileService.MakeDirectory(relativeTargetFolder);
             _fileService.Move(relativeSourcePath, relativeTargetPath);
             stream.Dispose();
             headerStream.Dispose();
-
-
+            
             metaData.SEQ = ID;
+            metaData.USER_ID = userId;
             metaData.AUDIO_FORMAT = audioFormat;
+            metaData.FILE_SIZE = _fileService.GetFileSize(relativeTargetPath);
             metaData.FILE_PATH = @$"\\{_fileService.UploadHost}\{relativeTargetPath}";
-            _dal.Insert(userId, metaData);
+            _dal.Insert(metaData);
             return ID;
         }
     }
