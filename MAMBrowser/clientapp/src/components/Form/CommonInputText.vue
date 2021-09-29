6f2b3daf9820973eeec146ba26a6fb492032dce8
daesik.choi <74227910+cds9046@users.noreply.github.com>
2021-09-29 16:32:41 +0900

    input box 검색 오류 수정

--- MAMBrowser/clientapp/src/components/Form/CommonInputText.vue
+++ MAMBrowser/clientapp/src/components/Form/CommonInputText.vue
@@ -1,51 +1,48 @@
 <template>
-    <b-form-input
-        ref="input"
-        :class="[classString]"
-        :value="value"
-        :placeholder="placeholder"
-        @input="onInput"
-        :pattern="pattern"
-        @keyup.enter="enterKeydown"
-        @keydown.prevent
-    >
-    </b-form-input>
+  <b-form-input
+    ref="input"
+    :class="[classString]"
+    :value="value"
+    :placeholder="placeholder"
+    @input="onInput"
+    :pattern="pattern"
+    @keyup.enter="enterKeydown"
+    @keydown.enter.prevent
+  >
+  </b-form-input>
 </template>
 
 <script>
 export default {
-    props: {
-        value: {
-            type: String,
-            default: '',
-        },
-        type: {
-            type: String,
-            default: 'text',
-        },
-        classString: {
-            type: String,
-            default: '',
-        },
-        placeholder: {
-            type: String,
-            default: '',
-        },
-        pattern: {
-            type: String,
-            default: '',
-        },
+  props: {
+    value: {
+      type: String,
+      default: ""
     },
-    methods: {
-        test(){
-            console.log("hi");
-        },
-        enterKeydown(){
-            this.$emit('keydown');
-        },
-        onInput(input) {
-            this.$emit('input', input);
-        },
+    type: {
+      type: String,
+      default: "text"
+    },
+    classString: {
+      type: String,
+      default: ""
+    },
+    placeholder: {
+      type: String,
+      default: ""
+    },
+    pattern: {
+      type: String,
+      default: ""
     }
-}
+  },
+  methods: {
+    enterKeydown() {
+      this.$emit("keydown");
+    },
+    onInput(input) {
+      this.$emit("input", input);
+    }
+  }
+};
 </script>
