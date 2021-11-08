0258bf79cc7956b83c5fa23862eceaa8530a9a97
dongwoo.lee <dongwoo.lee@adsoft.kr>
2021-11-08 14:57:44 +0900

    마스터링 함수 생성

---
+++ MAMBrowser/MAMDto/MasteringMetaBase.cs
@@ -0,0 +1,14 @@
+﻿using static MAMBrowser.Foundation.MAMDefine;
+
+namespace MAMBrowser.MAMDto
+{
+     public class MasteringMetaBase
+    {
+        public SoundDataTypes SoundType { get; set; }
+        public string UserId { get; set; }
+        /// <summary>
+        /// 등록일시 : ex)yyyy-MM-dd HH:mm:ss
+        /// </summary>
+        public string RegDtm { get; set; }
+    }
+}
---
+++ MAMBrowser/MAMDto/MyDiskMeta.cs
@@ -0,0 +1,18 @@
+﻿using MAMBrowser.Foundation;
+
+namespace MAMBrowser.MAMDto
+{
+    public class MyDiskMeta : MasteringMetaBase
+    {
+        public string Title
+        {
+            get;
+            set;
+        }
+        public string Memo
+        {
+            get;
+            set;
+        }
+    }
+}
