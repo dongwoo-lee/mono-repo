2d83e678ec52604e00660823f09f0f98cbf52bc6
daesik.choi <74227910+cds9046@users.noreply.github.com>
2021-12-07 10:31:45 +0900

    - 참조 경로 오류 수정

--- MAMBrowser/BLL/MasteringCategory.cs
+++ MAMBrowser/BLL/MasteringCategory.cs
@@ -1,7 +1,6 @@
 ﻿using M30.AudioFile.Common.DTO;
 using M30.AudioFile.DAL;
 using M30.AudioFile.DAL.Dao;
-using M30.AudioFile.DAL.Repositories;
 using System.Collections.Generic;
 
 namespace MAMBrowser.BLL
