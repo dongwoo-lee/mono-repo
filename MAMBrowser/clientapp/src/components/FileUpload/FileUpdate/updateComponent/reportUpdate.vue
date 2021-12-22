7311f3836ead96a5efa8ea9ee2091238ed9d2174
daesik.choi <74227910+cds9046@users.noreply.github.com>
2021-12-22 11:21:47 +0900

    - 마스터링 취재물 메타데이터 수정 위젯 수정

--- MAMBrowser/clientapp/src/components/FileUpload/FileUpdate/updateComponent/reportUpdate.vue
+++ MAMBrowser/clientapp/src/components/FileUpload/FileUpdate/updateComponent/reportUpdate.vue
@@ -9,29 +9,7 @@
       @input="changeMedia"
     />
     <br />
-    <br />
-    <h6>방송일</h6>
-    <b-input-group class="mb-3" style="width: 350px; float: left">
-      <input
-        type="text"
-        class="form-control input-picker"
-        :value="brdDT"
-        @input="onInput"
-      />
-      <b-input-group-append>
-        <b-form-datepicker
-          :value="brdDT"
-          @input="eventInput"
-          button-only
-          button-variant="outline-dark"
-          right
-          aria-controls="example-input"
-          @context="onContext"
-        ></b-form-datepicker>
-      </b-input-group-append>
-    </b-input-group>
-    <br />
-    <br />
+
     <br />
     <h6>취재인</h6>
     <b-form-input
@@ -53,7 +31,17 @@
       placeholder="소재명"
       trim
     />
-
+    <br />
+    <h6>방송일</h6>
+    <b-form-input
+      style="width: 350px"
+      class="editTask"
+      :value="this.brdDT"
+      disabled
+      aria-describedby="input-live-help input-live-feedback"
+      placeholder="소재명"
+      trim
+    />
     <br />
     <h6>사용처</h6>
     <b-form-input
@@ -65,14 +53,20 @@
       placeholder="소재명"
       trim
     />
-    <b-button style="margin-left: 20px; margin-top: 10px" @click="getPro"
+
+    <b-button
+      variant="outline-primary"
+      style="margin-left: 20px; margin-top: 10px"
+      @click="getPro"
       >검색</b-button
     >
+
     <report-modal
       v-if="reportModal"
-      :pgmData="pgmData"
+      :rowData="rowData"
       @reportModalClose="reportModalOff"
       @changePgm="changePgm"
+      @changeBrdDT="changeBrdDT"
     ></report-modal>
   </div>
 </template>
@@ -95,10 +89,9 @@ export default {
       reportModal: false,
       media: this.rowData.categoryName,
       reporter: this.rowData.reporter,
-      tempDate: this.convertDateSTH(this.rowData.brdDT),
-      brdDT: this.convertDateSTH(this.rowData.brdDT),
       pgmName: this.rowData.pgmName,
       pgmid: this.rowData.pgmid,
+      brdDT: this.convertDateSTH(this.rowData.brdDT),
       name: this.rowData.name,
       mediaOptions: [],
       pgmData: [
@@ -121,16 +114,6 @@ export default {
           text: e.name,
         });
       });
-    });
-
-    const replaceVal = this.brdDT.replace(/-/g, "");
-    const yyyy = replaceVal.substring(0, 4);
-    const mm = replaceVal.substring(4, 6);
-    const dd = replaceVal.substring(6, 8);
-    var date = yyyy + "" + mm + "" + dd;
-    this.pgmData = [];
-    axios.get(`/api/categories/pgmcodes?brd_dt=${date}`).then((res) => {
-      this.pgmData = res.data.resultObject.data;
     });
 
     this.update();
@@ -159,6 +142,10 @@ export default {
       this.pgmid = v.pgmid;
       this.update();
     },
+    changeBrdDT(v) {
+      this.brdDT = v;
+      this.update();
+    },
     update() {
       var meta = {
         ID: this.rowData.id,
@@ -170,48 +157,6 @@ export default {
         name: this.name,
       };
       this.$emit("updateReportMeta", meta);
-    },
-    eventInput(event) {
-      this.brdDT = event;
-      this.tempDate = event;
-      this.update();
-    },
-    onInput(event) {
-      const targetValue = event.target.value;
-
-      const replaceAllTargetValue = targetValue.replace(/-/g, "");
-      if (this.validDateType(targetValue)) {
-        if (this.tempDate == null) {
-          event.target.value = this.convertDateSTH(this.rowData.brdDT);
-          return;
-        }
-
-        event.target.value = this.tempDate;
-
-        return;
-      }
-
-      if (!isNaN(replaceAllTargetValue)) {
-        if (replaceAllTargetValue.length === 8) {
-          const convertDate = this.convertDateSTH(replaceAllTargetValue);
-          if (
-            convertDate == "" ||
-            convertDate == null ||
-            convertDate == "undefined"
-          ) {
-            this.brdDT = this.convertDateSTH(this.rowData.brdDT);
-            this.tempDate = this.convertDateSTH(this.rowData.brdDT);
-            return;
-          }
-          this.brdDT = convertDate;
-          this.tempDate = convertDate;
-        }
-      }
-      this.update();
-    },
-    validDateType(value) {
-      const dateRegex = /^(\d{0,4})[-]?\d{0,2}[-]?\d{0,2}$/;
-      return !dateRegex.test(value);
     },
     convertDateSTH(value) {
       const replaceVal = value.replace(/-/g, "");
@@ -225,12 +170,6 @@ export default {
       } else {
         return `${yyyy}-${mm}-${dd}`;
       }
-    },
-    onContext(ctx) {
-      // The date formatted in the locale, or the `label-no-date-selected` string
-      this.formatted = ctx.selectedFormatted;
-      // The following will be an empty string until a valid date is entered
-      this.dateSelected = ctx.selectedYMD;
     },
   },
 };
