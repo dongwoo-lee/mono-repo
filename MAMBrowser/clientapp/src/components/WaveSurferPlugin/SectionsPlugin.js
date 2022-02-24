aa3d9f4750106f83b2e43cb767d6481c793216f3
dongwoo.lee <dongwoo.lee@adsoft.kr>
2022-02-24 14:15:18 +0900

    광고그룹미리듣기 브라우저 인포로그 제거

--- MAMBrowser/clientapp/src/components/WaveSurferPlugin/SectionsPlugin.js
+++ MAMBrowser/clientapp/src/components/WaveSurferPlugin/SectionsPlugin.js
@@ -5,7 +5,7 @@
  * @property {?label} string An optional section label
  * @property {?color} string Background color for section
  */
- 
+
 /**
  * Sections are points in time in the audio that can be jumped to.
  *
@@ -27,14 +27,14 @@
  *   ]
  * });
  */
- 
- export default class SectionsPlugin {
+
+export default class SectionsPlugin {
     /**
      * @typedef {Object} SectionsPluginParams
      * @property {?SectionParams[]} sections Initial set of sections
      * @fires SectionsPlugin#section-click
      */
- 
+
     /**
      * Sections plugin definition factory
      *
@@ -52,11 +52,9 @@
             params: params,
             staticProps: {
                 addSection(options) {
-                    console.info('options', options);
                     if (!this.initialisedPluginList.sections) {
                         this.initPlugin('sections');
                     }
-                    console.info('this', this);
                     return this.sections.add(options);
                 },
                 clearSections() {
@@ -66,10 +64,8 @@
             instance: SectionsPlugin
         };
     }
- 
+
     constructor(params, ws) {
-        console.info('cont params',params);
-        console.info('cont ws',ws);
         this.params = params;
         this.wavesurfer = ws;
         this.util = ws.util;
@@ -82,23 +78,23 @@
         this.wavesurfer.container.insertBefore(this.container, this.wavesurfer.container.firstChild);
 
         this.style(wrapper, {
-           position:"relative",
-           display:"block",
-           height:"25px",
-           // overflow:"hidden",
+            position: "relative",
+            display: "block",
+            height: "25px",
+            // overflow:"hidden",
         });
 
         this.style(this.container, {
-           // position:"relative",
-           // display:"block",
-           height:"25px",
-           display : "block",
-           overflow:"hidden",
+            // position:"relative",
+            // display:"block",
+            height: "25px",
+            display: "block",
+            overflow: "hidden",
         });
         this._onResize = () => {
             this._updateSectionPositions();
         };
- 
+
         this._onBackendCreated = () => {
             if (this.params.sections) {
                 this.params.sections.forEach(section => this.add(section));
@@ -107,7 +103,7 @@
             window.addEventListener('orientationchange', this._onResize, true);
             this.wavesurfer.on('zoom', this._onResize);
         };
- 
+
         this.sections = [];
         this._onReady = () => {
             ws.drawer.wrapper.addEventListener('scroll', this._onScroll);
@@ -115,10 +111,10 @@
         };
     }
     _onScroll = () => {
-       if (this.container) {
-           this.container.scrollLeft = this.wavesurfer.drawer.wrapper.scrollLeft;
-       }
-   };
+        if (this.container) {
+            this.container.scrollLeft = this.wavesurfer.drawer.wrapper.scrollLeft;
+        }
+    };
     init() {
         // Check if ws is ready
         if (this.wavesurfer.isReady) {
@@ -129,19 +125,19 @@
             this.wavesurfer.once('backend-created', this._onBackendCreated);
         }
     }
- 
+
     destroy() {
         this.wavesurfer.un('ready', this._onReady);
         this.wavesurfer.un('backend-created', this._onBackendCreated);
- 
+
         this.wavesurfer.un('zoom', this._onResize);
- 
+
         window.removeEventListener('resize', this._onResize, true);
         window.removeEventListener('orientationchange', this._onResize, true);
- 
+
         this.clear();
     }
- 
+
     /**
      * Add a section
      *
@@ -149,22 +145,21 @@
      * @return {object} The created section
      */
     add(params) {
-        console.info('params',params);
         let section = {
             startTime: params.startTime,
             endTime: params.endTime,
             label: params.label,
             color: params.color,
         };
- 
+
         section.el = this._createSectionElement(section);
         this.wrapper.insertBefore(section.el, this.wrapper.firstChild);
         this.sections.push(section);
         this._updateSectionPositions();
- 
+
         return section;
     }
- 
+
     /**
      * Remove a section
      *
@@ -175,82 +170,82 @@
         if (!section) {
             return;
         }
- 
+
         this.wrapper.removeChild(section.el);
         this.sections.splice(index, 1);
     }
- 
+
     _createSectionElement(section) {
         const el = document.createElement('div');
         this.style(el, {
-            "display":"flex",
-            "position":"absolute",
-            "background-color": "rgba(255,255,0,0.15)", 
+            "display": "flex",
+            "position": "absolute",
+            "background-color": "rgba(255,255,0,0.15)",
             width: "1200px",
-            "z-index":6,       
-            height:"25px",
-            "color" : "black",
-            border : "1px solid"
+            "z-index": 6,
+            height: "25px",
+            "color": "black",
+            border: "1px solid"
         });
 
         const textEle = document.createElement('div');
         textEle.textContent = section.label;
         textEle.title = section.label;
         this.style(textEle, {
-           "position":"sticky",
-           left:"15px",
-           overflow:"hidden",
-           "text-overflow":"ellipsis",
-           "white-space":"nowrap",
-           cursor : "default"
-       });
+            "position": "sticky",
+            left: "15px",
+            overflow: "hidden",
+            "text-overflow": "ellipsis",
+            "white-space": "nowrap",
+            cursor: "default"
+        });
 
         el.insertBefore(textEle, el.firstChild);
         return el;
     }
     getWidth() {
-       return this.wavesurfer.drawer.width / this.wavesurfer.params.pixelRatio;
+        return this.wavesurfer.drawer.width / this.wavesurfer.params.pixelRatio;
     }
     _updateSectionPositions() {
-       const dur = this.wavesurfer.getDuration();
-       const width = this.getWidth();
-       
+        const dur = this.wavesurfer.getDuration();
+        const width = this.getWidth();
 
-       for ( let i = 0 ; i < this.sections.length; i++ ) {
-           let startLimited = this.sections[i].startTime;
-           let endLimited = this.sections[i].endTime;
-           if (startLimited < 0) {
-               startLimited = 0;
-               endLimited = endLimited - startLimited;
-           }
-           if (endLimited > dur) {
-               endLimited = dur;
-               startLimited = dur - (endLimited - startLimited);
-           }
-           if (this.sections[i].el != null) {
-               // Calculate the left and width values of the region such that
-               // no gaps appear between regions.
-               const left = Math.round((startLimited / dur) * width);
-               const regionWidth = Math.round((endLimited / dur) * width) - left;
 
-               this.style(this.sections[i].el, {
-                   left: left + 'px',
-                   width: regionWidth + 'px',
-                   // backgroundColor: this.sections[i].color,
-               });
+        for (let i = 0; i < this.sections.length; i++) {
+            let startLimited = this.sections[i].startTime;
+            let endLimited = this.sections[i].endTime;
+            if (startLimited < 0) {
+                startLimited = 0;
+                endLimited = endLimited - startLimited;
+            }
+            if (endLimited > dur) {
+                endLimited = dur;
+                startLimited = dur - (endLimited - startLimited);
+            }
+            if (this.sections[i].el != null) {
+                // Calculate the left and width values of the region such that
+                // no gaps appear between regions.
+                const left = Math.round((startLimited / dur) * width);
+                const regionWidth = Math.round((endLimited / dur) * width) - left;
 
-               this.style(this.wrapper, {
-                   width : this.wavesurfer.drawer.width + 'px'
+                this.style(this.sections[i].el, {
+                    left: left + 'px',
+                    width: regionWidth + 'px',
+                    // backgroundColor: this.sections[i].color,
                 });
-           }
-       }
+
+                this.style(this.wrapper, {
+                    width: this.wavesurfer.drawer.width + 'px'
+                });
+            }
+        }
     }
- 
+
     /**
      * Remove all sections
      */
     clear() {
-        while ( this.sections.length > 0 ) {
+        while (this.sections.length > 0) {
             this.remove(0);
         }
     }
