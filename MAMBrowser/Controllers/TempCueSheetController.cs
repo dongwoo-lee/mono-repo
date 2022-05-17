58e6702e6311c1fd926356d13f75c07ab6c731d0
eunbee.kim <eunbee.kim@adsoft.kr>
2022-05-17 10:05:31 +0900

    template controller 작업 중

--- MAMBrowser/Controllers/TempCueSheetController.cs
+++ MAMBrowser/Controllers/TempCueSheetController.cs
@@ -6,6 +6,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using M30.AudioFile.Common.DTO;
 
 namespace MAMBrowser.Controllers
 {
@@ -22,20 +23,20 @@ namespace MAMBrowser.Controllers
 
         // 템플릿 목록 가져오기
         [HttpGet("GetTempList")]
-        public TempCueList_Result GetTempList([FromQuery] string personid, string title, int row_per_page, int select_page)
+        public DTO_RESULT<TempCueList_Page> GetTempList([FromQuery] string personid, string title, int row_per_page, int select_page)
         {
+            var result = new DTO_RESULT<TempCueList_Page>();
             try
             {
-                var result = new TempCueList_Result();
-                result.ResultObject = new TempCueList_Page();
                 result.ResultObject = _bll.GetPersonIDWithTitleTemplateList(personid, title, row_per_page, select_page);
                 result.ResultCode = RESUlT_CODES.SUCCESS;
-                return result;
             }
-            catch
+            catch(Exception ex)
             {
-                throw;
+                result.ErrorMsg = ex.Message;
+                result.ResultCode = RESUlT_CODES.SERVICE_ERROR;
             }
+                return result;
 
         }
 
