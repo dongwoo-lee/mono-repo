ceb58aeccb0e1bddb6062234f65c9f4d3a275ff9
daesik.choi <74227910+cds9046@users.noreply.github.com>
2023-11-30 12:24:59 +0900

    - 관리자 모니터링 페이지 수정

--- MAMBrowser/clientapp/src/views/app/config/monitoring/index.vue
+++ MAMBrowser/clientapp/src/views/app/config/monitoring/index.vue
@@ -2,10 +2,7 @@
   <div>
     <b-row>
       <b-colxx xxs="12">
-        <piaf-breadcrumb
-          heading="관리자 모니터링"
-          :noNav="true"
-        />
+        <piaf-breadcrumb heading="관리자 모니터링" :noNav="true" />
         <div class="separator mb-3"></div>
       </b-colxx>
     </b-row>
@@ -13,27 +10,14 @@
       <b-colxx>
         <b-card>
           <b-container fluid>
-            <b-tabs
-              content-class="mt-3"
-              fill
-            >
+            <b-tabs content-class="mt-3" fill>
               <!-- <b-tab
                 title="DL3"
                 @click="onTab('dl3')"
               /> -->
-              <b-tab
-                title="SLAP"
-                @click="onTab('slap')"
-                active
-              />
-              <b-tab
-                title="일반"
-                @click="onTab('etc')"
-              />
-              <b-tab
-                title="장비 관리"
-                @click="onTab('setting')"
-              />
+              <b-tab title="SLAP" @click="onTab('slap')" active />
+              <b-tab title="일반" @click="onTab('etc')" />
+              <b-tab title="장비 관리" @click="onTab('setting')" />
               <component :is="tabName"></component>
             </b-tabs>
           </b-container>
@@ -45,9 +29,9 @@
 
 <script>
 // import Dl3 from "../template/monitor_DL3";
-import Slap from "../template/monitor_SLAP";
-import Etc from "../template/monitor_ETC";
-import Setting from "../template/monitor_SETTING";
+import Slap from "../template/monitor_SLAP"
+import Etc from "../template/monitor_ETC"
+import Setting from "../template/monitor_SETTING"
 
 export default {
   components: { Slap, Etc, Setting },
@@ -55,12 +39,12 @@ export default {
     return {
       // tabName: "dl3",
       tabName: "slap",
-    };
+    }
   },
   methods: {
     onTab(tabName) {
-      this.tabName = tabName;
+      this.tabName = tabName
     },
   },
-};
+}
 </script>
