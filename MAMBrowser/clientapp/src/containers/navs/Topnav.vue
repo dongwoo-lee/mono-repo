248da4f9b8d91ea65f6dfb563341d12348e0f89c
eunbee.kim <eunbee.kim@adsoft.kr>
2024-01-11 11:00:13 +0900

    버전 업데이트

--- MAMBrowser/clientapp/src/containers/navs/Topnav.vue
+++ MAMBrowser/clientapp/src/containers/navs/Topnav.vue
@@ -110,7 +110,7 @@
                         margin-right: 6px;
                       "
                     >
-                      v1.1.231226
+                      v1.1.240111
                     </div>
                   </template>
                   <div v-if="isDisplaySetting()">
